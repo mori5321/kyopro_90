2c4af12da084a025ffdc6fa3d506e72ddd4c6439
mori5321 <moriwm77@gmail.com>
2021-06-03 23:01:28 +0900

    feat: 001

---
+++ src/yokan_001.rs
@@ -0,0 +1,111 @@
+fn solve(m: &isize, diffs: &Vec<isize>) -> isize {
+    let mut stack = 0;
+    let mut count = 0;
+
+    for (_, diff) in diffs.iter().enumerate() {
+        stack += diff;
+
+        if stack >= *m {
+            stack = 0;
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+fn main() {
+    let input = read_vec::<isize>();
+    let _n = input.get(0).unwrap(); // N個の切れ目
+    let l = input.get(1).unwrap(); // 長さL[cm]の羊羹
+
+    let k = read::<isize>(); // K個の切れ目を選ぶ
+    let xs = read_vec::<isize>(); // N個の切れ目の位置[cm]
+
+    let mut diffs: Vec<isize> = vec![];
+    for (i, x) in xs.iter().enumerate() {
+        let diff: isize;
+        if i == 0 {
+            diff = x.clone()
+        } else {
+            diff = x - xs.get(i - 1).unwrap();
+        }
+
+        diffs.push(diff);
+
+        if i == xs.len() - 1 {
+            let last_diff = l - x;
+            diffs.push(last_diff);
+        }
+    }
+
+    // println!("N: {}, L: {}", n, l);
+    // println!("K :{}", k);
+    // println!("Xs: {:?}", xs);
+    // println!("Diffs: {:?}", diffs);
+    //
+    let mut left: isize = 0;
+    let mut right: isize = l.clone();
+    // //
+    while (right - left) > 1 {
+        let mid: isize = left + ((right - left) / 2);
+
+        // println!("=============");
+        // println!("Left {}, Right: {}", left, right);
+        // println!("K: {}, Mid: {}, Solve: {}", k, mid, solve(&mid, &diffs));
+        // println!("Result: {}", solve(&mid, &diffs) > k);
+        if solve(&mid, &diffs) > k {
+            // println!("Go Right");
+            left = mid
+        } else {
+            // println!("Go Left");
+            right = mid
+        }
+    }
+    //
+    println!("{}", left);
+    // println!("Right: {}", right);
+    // let mut mid = left + (right + left) / 2;
+    //
+    // if ()
+
+    // println!("Mid / idx: {}, Value: {}", mid, xs[mid]);
+}
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+// fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+//     (0..n).map(|_| read_vec()).collect()
+// }
+//
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+    #[test]
+    fn test_solve() {
+        let vec = vec![1, 2, 3, 4];
+        let m = 3;
+
+        let result = solve(&m, &vec);
+
+        assert_eq!(3, result);
+
+        let vec2 = vec![4, 8, 10, 2, 3];
+        let m2 = 10;
+        let result2 = solve(&m2, &vec2);
+
+        assert_eq!(2, result2);
+    }
+}
